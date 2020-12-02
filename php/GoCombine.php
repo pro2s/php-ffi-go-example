0ec1042bef4bee0a452693e64c01f1538d0dcbf1
Sergey Protasov <s.protasov@itransition.com>
2020-12-02 18:37:40 +0300

    Refactor

--- php/GoCombine.php
+++ php/GoCombine.php
@@ -6,18 +6,17 @@ class GoCombine
 {
     protected $model;
     protected $ffi;
+    private const DEFF = <<<C
+        typedef struct { char* p; long n } GoString;
+        typedef struct { long id; long linked_id; bool empty; } Option;
+        typedef struct { void *data; long len; long cap; } GoSlice;
+        char* combine(long id, GoSlice data);
+    C;
 
-    public function __construct($model, $lib)
+    public function __construct(Model $model, string $lib)
     {
         $this->model = $model;
-        $this->ffi = \FFI::cdef(
-            "
-            typedef struct { char* p; long n } GoString;
-            typedef struct { long id; long linked_id; bool empty; } Option;
-            typedef struct { void *data; long len; long cap; } GoSlice;
-            char* combine(long id, GoSlice data);",
-            $lib
-        );
+        $this->ffi = \FFI::cdef(self::DEFF, $lib);
     }
 
     public function combine($combinations)
@@ -28,33 +27,51 @@ class GoCombine
         return unserialize($data);
     }
 
+    private function newArray($type, $len)
+    {
+        return $this->ffi->new(\FFI::arrayType($this->ffi->type($type), [$len]), false);
+    }
+
+    private function getPtr($data)
+    {
+        return \FFI::cast(\FFI::type('void *'), $data);
+    }
+
+    private function fillGoSlice(\FFI\CData $slice, \FFI\CData $data, int $length)
+    {
+        $slice->data = $this->getPtr($data);
+        $slice->len = $length;
+        $slice->cap = $length;
+    }
+
+    private function fillOption(\FFI\CData $data, ?Option $option)
+    {
+        if ($option === null) {
+            $data->empty = true;
+            return;
+        }
+
+        $data->id = $option->id;
+        $data->linked_id = $option->linked_id ?? 0;
+    }
+
     public function optionsToGoSlice(array $combinations): \FFI\CData
     {
         $dataLen = count($combinations);
-        $data = $this->ffi->new('GoSlice[' . $dataLen . ']', false);
+        $data = $this->newArray('GoSlice', $dataLen);
 
-        foreach ($combinations as $di => $options) {
+        foreach ($combinations as $i => $options) {
             $len = count($options);
-            $arr = $this->ffi->new('Option[' . $len . ']', false);
-            foreach ($options as $i => $option) {
-                if ($option === null) {
-                    $arr[$i]->empty = true;
-                    continue;
-                }
-
-                $arr[$i]->id = $option->id;
-                $arr[$i]->linked_id = $option->linked_id ?? 0;
+            $arr = $this->newArray('Option', $len);
+            foreach ($options as $j => $option) {
+                $this->fillOption($arr[$j], $option);
             }
 
-            $data[$di]->data = \FFI::cast(\FFI::type('void *'), $arr);
-            $data[$di]->len = $len;
-            $data[$di]->cap = $len;
+            $this->fillGoSlice($data[$i], $arr, $len);
         }
 
         $out = $this->ffi->new('GoSlice', false);
-        $out->data = \FFI::cast(\FFI::type('void *'), $data);
-        $out->len = $dataLen;
-        $out->cap = $dataLen;
+        $this->fillGoSlice($out, $data, $dataLen);
 
         return $out;
     }
